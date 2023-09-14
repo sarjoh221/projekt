e5725ef7a090f95877fa07b2f1e87ccdc35f3a32
jesgus <solsaxen@hotmail.com>
2023-09-14 17:13:06 +0200

    allt möjligt
    hejhej

--- projektet/app.js
+++ projektet/app.js
@@ -1,8 +1,49 @@
 const app = Vue.createApp({
   data() {
     return {
-     message: 'hej'
+      message: 'hej',
+      tavlor: [
+        {
+          bild: 'oga.png',
+          beskrivning: 'En tavla som går hem hos både stora och små. Ett lugnt vakande öga när stressen smyger sig på eller sömnen kallar. Vi förstår exakt varför denna är en storsäljare',
+          pris: 1250
+        },
+        {
+          bild: 'oga.png',
+          beskrivning: 'En tavla som går hem hos både stora och små. Ett lugnt vakande öga när stressen smyger sig på eller sömnen kallar. Vi förstår exakt varför denna är en storsäljare',
+          pris: 1250
+        },
+        {
+          bild: 'oga.png',
+          beskrivning: 'Beskrivning av denna fina tavla',
+          pris: 1250
+        },
+        {
+          bild: 'oga.png',
+          beskrivning: 'En tavla som går hem hos både stora och små. Ett lugnt vakande öga när stressen smyger sig på eller sömnen kallar. Vi förstår exakt varför denna är en storsäljare',
+          pris: 1250
+        },
+        {
+          bild: 'oga.png',
+          beskrivning: 'En tavla som går hem hos både stora och små. Ett lugnt vakande öga när stressen smyger sig på eller sömnen kallar. Vi förstår exakt varför denna är en storsäljare',
+          pris: 1250
+        },
+        {
+          bild: 'oga.png',
+          beskrivning: 'En tavla som går hem hos både stora och små. Ett lugnt vakande öga när stressen smyger sig på eller sömnen kallar. Vi förstår exakt varför denna är en storsäljare',
+          pris: 1250
+        },
+        {
+          bild: 'oga.png',
+          beskrivning: 'Beskrivning av denna fina tavla',
+          pris: 1250
+        }
+
+      ]
+
     }
+
+
   },
   methods: {
   },
